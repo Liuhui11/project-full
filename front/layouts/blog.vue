17b0631341c1f83e4e2ce1c75af0ddb11ced0795
liuhui <liuhuigoing@gmail.com>
2020-07-09 23:14:06 +0800

    first commit

---
+++ front/layouts/blog.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>我的博客导航在这里</div>
+    <nuxt/>
+  </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="stylus">
+
+</style>
---
+++ front/pages/posts.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>文章1</h1>
+  </div>
+</template>
+<script>
+export default {
+    layout: 'blog'
+}
+</script>
+<style lang="stylus">
+
+</style>
